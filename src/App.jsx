1f21a836d8866d4ad2df637bbc6d3284ac0a56c0
GaneshAher1997 <aherganesh194@gmail.com>
2024-07-29 18:01:59 +0530

    1.create series netflix page

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+// import React from "react";
+// export const App = () => {
+//   return <h1>Hello, Thapa Technical.</h1>;
+// }
+
+// console.log(React.createElement("h1", null, "Hello,Thapa Technical."));
+
+
+export const App = () => 
+  {
+  return(
+    <div>
+      <div>
+      <img src="om/wp-content/uploads/2024/02/kim-soo-hyun-kim-ji-won-queen-of-tears-1-696x442.jpg" 
+      alt="Queen of Tears"   
+      width="40%" hight="40%"/>
+      </div>
+<h2>Name: Queen of Tears</h2>
+<h3>Rating:8.2</h3>
+<p>sumamry:Baek Hyun-woo and hong hae-in navigate a tense relationship,
+     both at home at a work. but upon deciding his future, 
+    hun-woo pas a visitto his family.  
+    </p>
+    </div>
+  );
+};
