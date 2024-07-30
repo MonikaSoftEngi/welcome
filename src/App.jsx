acd175837b9f8be16e45e438afa50dc19059d902
GaneshAher1997 <aherganesh194@gmail.com>
2024-07-30 15:06:54 +0530

    1.dynamic value in jsx create. 2.use of rating.

--- src/App.jsx
+++ src/App.jsx
@@ -25,34 +25,66 @@
 //   );
 // };
 
-import {Fregment} from "react";
-export const App = () => {
-  return(
+// import {Fregment} from "react";
+// export const App = () => {
+//   return(
     
-     <Fragment>
-      <NetflixSeries/>
-      <NetflixSeries/>
-      <NetflixSeries/>
-      <NetflixSeries/>
-      <NetflixSeries/>
-      </Fragment>
+//      <Fragment>
+//       <NetflixSeries/>
+//       <NetflixSeries/>
+//       <NetflixSeries/>
+//       <NetflixSeries/>
+//       <NetflixSeries/>
+//       </Fragment>
+//   );
+// };
+// const NetflixSeries = () => 
+//   {
+//     return(
+//       <div>
+//         <div>
+//         <img src="om/wp-content/uploads/2024/02/kim-soo-hyun-kim-ji-won-queen-of-tears-1-696x442.jpg" 
+//         alt="Queen of Tears"   
+//         width="40%" hight="40%"/>
+//         </div>
+//   <h2>Name: Queen of Tears</h2>
+//   <h3>Rating:8.2</h3>
+//   <p>sumamry:Baek Hyun-woo and hong hae-in navigate a tense relationship,
+//        both at home at a work. but upon deciding his future, 
+//       hun-woo pas a visitto his family.  
+//       </p>
+//       </div>
+//     );
+//   };
+
+export const App = ()  => {
+  retuen (
+    <>
+    <NetflixSeries />
+    <NetflixSeries />
+    <NetflixSeries />
+    <NetflixSeries />
+    <NetflixSeries />
+    </>
   );
 };
-const NetflixSeries = () => 
-  {
-    return(
-      <div>
-        <div>
-        <img src="om/wp-content/uploads/2024/02/kim-soo-hyun-kim-ji-won-queen-of-tears-1-696x442.jpg" 
-        alt="Queen of Tears"   
-        width="40%" hight="40%"/>
-        </div>
-  <h2>Name: Queen of Tears</h2>
-  <h3>Rating:8.2</h3>
-  <p>sumamry:Baek Hyun-woo and hong hae-in navigate a tense relationship,
-       both at home at a work. but upon deciding his future, 
-      hun-woo pas a visitto his family.  
-      </p>
-      </div>
-    );
-  };
+
+const NetflixSeries = () => {
+const name = "Queen of Tears";
+const rating = "8.2";
+const summary =
+"Baek Hyun-woo and hong hae-in navigate a tense relationship,";
+      //  both at home at a work. but upon deciding his future, 
+      // hun-woo pas a visitto his family.";
+return (
+  <div>
+    <div>
+      <img src ="om/wp-content/uploads/2024/02/kim-soo-hyun-kim-ji-won-queen-of-tears-1-696x442.jpg" 
+      alt="Queen of Tears" width="40%"hight="40%" />
+    </div>
+    <h2>Name: {name} </h2>
+    <h3>Rating: {5 + 3.2}</h3>
+    <p>Sumamry: {Summary}</p>
+  </div>
+);
+};
