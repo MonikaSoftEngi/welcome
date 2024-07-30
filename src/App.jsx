33559a3b8ef18969740cb24f262da09d2f330d4c
GaneshAher1997 <aherganesh194@gmail.com>
2024-07-30 09:21:15 +0530

    1. freagment

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,53 @@
 // console.log(React.createElement("h1", null, "Hello,Thapa Technical."));
 
 
-export const App = () => 
-  {
+// export const App = () => 
+// {
+//   return(
+//     <div>
+//       <div>
+//       <img src="om/wp-content/uploads/2024/02/kim-soo-hyun-kim-ji-won-queen-of-tears-1-696x442.jpg" 
+//       alt="Queen of Tears"   
+//       width="40%" hight="40%"/>
+//       </div>
+// <h2>Name: Queen of Tears</h2>
+// <h3>Rating:8.2</h3>
+// <p>sumamry:Baek Hyun-woo and hong hae-in navigate a tense relationship,
+//      both at home at a work. but upon deciding his future, 
+//     hun-woo pas a visitto his family.  
+//     </p>
+//     </div>
+//   );
+// };
+
+import {Fregment} from "react";
+export const App = () => {
   return(
-    <div>
-      <div>
-      <img src="om/wp-content/uploads/2024/02/kim-soo-hyun-kim-ji-won-queen-of-tears-1-696x442.jpg" 
-      alt="Queen of Tears"   
-      width="40%" hight="40%"/>
-      </div>
-<h2>Name: Queen of Tears</h2>
-<h3>Rating:8.2</h3>
-<p>sumamry:Baek Hyun-woo and hong hae-in navigate a tense relationship,
-     both at home at a work. but upon deciding his future, 
-    hun-woo pas a visitto his family.  
-    </p>
-    </div>
+    
+     <Fragment>
+      <NetflixSeries/>
+      <NetflixSeries/>
+      <NetflixSeries/>
+      <NetflixSeries/>
+      <NetflixSeries/>
+      </Fragment>
   );
 };
+const NetflixSeries = () => 
+  {
+    return(
+      <div>
+        <div>
+        <img src="om/wp-content/uploads/2024/02/kim-soo-hyun-kim-ji-won-queen-of-tears-1-696x442.jpg" 
+        alt="Queen of Tears"   
+        width="40%" hight="40%"/>
+        </div>
+  <h2>Name: Queen of Tears</h2>
+  <h3>Rating:8.2</h3>
+  <p>sumamry:Baek Hyun-woo and hong hae-in navigate a tense relationship,
+       both at home at a work. but upon deciding his future, 
+      hun-woo pas a visitto his family.  
+      </p>
+      </div>
+    );
+  };
