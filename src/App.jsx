476e987e988e0ce13a11a37a9d62bb52a50732d3
GaneshAher1997 <aherganesh194@gmail.com>
2024-07-30 17:04:57 +0530

    1. conditional rendering in jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -76,6 +76,27 @@ const summary =
 "Baek Hyun-woo and hong hae-in navigate a tense relationship,";
       //  both at home at a work. but upon deciding his future, 
       // hun-woo pas a visitto his family.";
+      let age = 1;
+      const returnGanre  = () => {
+        const genre = "RomCom";
+        return genre;
+
+      };
+      if(age < 18){
+        return (
+          <div>
+            <div>
+              <img src ="om/wp-content/uploads/2024/02/kim-soo-hyun-kim-ji-won-queen-of-tears-1-696x442.jpg" 
+              alt="Queen of Tears" width="40%"hight="40%" />
+            </div>
+            <h2>Name: {name} </h2>
+            <h3>Rating: {5 + 3.2}</h3>
+            <p>Sumamry: {Summary}</p>
+            <button>not available</button>
+          </div>
+        );
+        };
+      
 return (
   <div>
     <div>
@@ -85,6 +106,7 @@ return (
     <h2>Name: {name} </h2>
     <h3>Rating: {5 + 3.2}</h3>
     <p>Sumamry: {Summary}</p>
+    <button>watch now</button>
   </div>
 );
 };
