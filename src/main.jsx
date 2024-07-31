fe56e60dbc463556f2d6ee0c9b6baef0bfac43dd
GaneshAher1997 <aherganesh194@gmail.com>
2024-07-31 14:30:05 +0530

    1.create main.jsx.

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDom from "React-dom/client";
+import { practices } from "./practices.jsx";
+
+ReactDom.createRoot(document.getElementById("root")).render(
+    <React.Strictmode>
+        {}
+        <Practices/>
+        </React.Strictmode>
+);
