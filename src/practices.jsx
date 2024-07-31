c88142327cb5900775002f5243163f38ec351fe2
GaneshAher1997 <aherganesh194@gmail.com>
2024-07-31 14:49:04 +0530

    1.practices use in jsx.

---
+++ src/practices.jsx
@@ -0,0 +1,11 @@
+export const practices = () => {
+   const students = [];
+
+ return (
+<>
+    
+        <p>{student.length && "No students found"}</p>
+        <p>Number of student: {student.length}</p>
+    </>
+ );
+};
