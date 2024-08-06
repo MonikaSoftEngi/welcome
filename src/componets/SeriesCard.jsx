61f448ce9cc90435e8046b9215f93969495b4a99
GaneshAher1997 <aherganesh194@gmail.com>
2024-08-06 13:38:20 +0530

    1. inline css.

--- src/componets/SeriesCard.jsx
+++ src/componets/SeriesCard.jsx
@@ -1,19 +1,27 @@
 export const SeriesCard = ({data}) => {
-  const { id, img_url, name, rating, description, cast, genre, watch_url } =
+  const {  img_url, name, rating, description, cast, genre, watch_url } =
     data;
+    const btn_style ={padding:"1.2rem 2.4rem", 
+      border:"none",
+      fontsize:"1.6rem",
+      backgroundcolor:"var(--bnt-hover-bg-color)",
+      color:"var(--bg-color)",}
   return (
-    <li>
+    <li class Name="card">
       <div>
       <img src={img_url} alt={name} width="40%" hight="40%" />
       </div>
+      <div className="card-content">
       <h2>Name: {name} </h2>
       <h3>Rating:{Rating} </h3>
-      <p>summary:{summary} </p>
-      <p>Genre:{Genre} </p>
-      <p>Cast:{Cast} </p>
+      <p>summary:{description} </p>
+      <p>Genre:{Genre.join} </p>
+      <p>Cast:{Cast.join} </p>
       <a href={Watch_url} target="_blank">
-      <button>Watch Now</button>
+      <button style={btn_style} >
+      Watch Now</button>
       </a>
+      </div>
     </li>
   );
 };
