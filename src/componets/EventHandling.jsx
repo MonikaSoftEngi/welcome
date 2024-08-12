6808dfc3a1fcbb40bdc8eb16889025204212c18a
GaneshAher1997 <aherganesh194@gmail.com>
2024-08-12 18:37:27 +0530

    1. inline event . 2 inline aarow function. 3 passing argument.

--- src/componets/EventHandling.jsx
+++ src/componets/EventHandling.jsx
@@ -1,11 +1,44 @@
 import "./Ev.css";
 export const EventHandling = () => {
-    function handleButtonClick(){
+    // function handleButtonClick(){
+    //     alert("Hey i am onClick Event");
+    // }
+    const handelButtonClick = (event) => {
+        console.log(event);
+        console.log(event.target);
+        console.log(event.type);
         alert("Hey i am onClick Event");
+    };
+    const Handlewelcomeuser = (User)=> {
+        console.log(`"Hey ${user},Welcome"`);
     }
     return (
         <>
+        {/* function componets with named function */}
         <button onClick={handelButtonCick}>click me</button>
+        <br/>
+        <button onClick={(event) =>handelButtonCick(event)}>click me 2</button>
+        <br/>
+        {/* inline event handlers */}
+        <button onClick={(event)=>console.log(event)}>Inline function</button>
+        <br/>
+        {/* function componets with inline arrow function */}
+        <button onClick={() =>alert("Hey I am event function")}>Inline Arr fun
+        </button>
+        {/* passing argument to event handlers */}
+        {/* <button onClick ={handelWelcomeUser("Monika")}>Click me</button> */}
+        <button onClick ={() =>handelWelcomeUser("Aarohi")}>Click me</button>
         </>
     );
 };
+
+
+
+
+
+
+
+
+
+
+
