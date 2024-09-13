49f2501d44196e44bd4eb7278c6972be0af1a78d
MonikaSoftEngi <ahermonika31@gmail.com>
2024-09-13 18:35:18 +0530

    1.mini project button on/of.

--- src/projects/ToggleSwitch/TogglSwitch.jsx
+++ src/projects/ToggleSwitch/TogglSwitch.jsx
@@ -3,10 +3,17 @@ import "./ToggleSwitch.css";
 export const ToggleSwitch = () => {
   const [isOn, setIsOn] = usestate(false);
 
+  const handleToggleSwitch = () => {
+    setIson(!isOn);
+  };
+const checkIson =ison ? "on" : "off";
+const toggleBGColor = {backgroundcolor:isOn ? "#4caf50" : "#f44336"}
   return (
-    <div className="toggle-switch">
-      <div className="switch">
-        <span className="Switch-state"> {isOn ? "On" :"off"}  </span>
+    <div className="toggle-switch"  
+    style ={toggleBGColor}  
+    onClick={handleToglleSwitch}>
+      <div className= {`switch ${checkIson}`}>
+        <span className="Switch-state"> {checkIson} </span>
       </div>
     </div>
   );
