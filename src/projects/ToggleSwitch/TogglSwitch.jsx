66c34e29463ce0aed305c581d194e597ddfbe417
MonikaSoftEngi <ahermonika31@gmail.com>
2024-09-23 13:26:31 +0530

    1. react icons.

--- src/projects/ToggleSwitch/TogglSwitch.jsx
+++ src/projects/ToggleSwitch/TogglSwitch.jsx
@@ -1,5 +1,8 @@
-import "./ToggleSwitch.css";
+import {useState} from "react";
 
+import "./ToggleSwitch.css";
+import {IoIosSwitch} from "react-icond/io";
+import {piUserSwitchFill} from "react-icons/pi";
 export const ToggleSwitch = () => {
   const [isOn, setIsOn] = usestate(false);
 
@@ -9,6 +12,12 @@ export const ToggleSwitch = () => {
 const checkIson =ison ? "on" : "off";
 const toggleBGColor = {backgroundcolor:isOn ? "#4caf50" : "#f44336"}
   return (
+    <>
+    <h1  style={{ color:"#000", textAlign:"center"}}>
+      Toggle Switch  {" "}
+      <IoIosSwitch style={{ color:"red", textAlign:"center"}}   />
+      <piUserSwitchFill/>
+      </h1>
     <div className="toggle-switch"  
     style ={toggleBGColor}  
     onClick={handleToglleSwitch}>
@@ -16,5 +25,6 @@ const toggleBGColor = {backgroundcolor:isOn ? "#4caf50" : "#f44336"}
         <span className="Switch-state"> {checkIson} </span>
       </div>
     </div>
+    </>
   );
 };
