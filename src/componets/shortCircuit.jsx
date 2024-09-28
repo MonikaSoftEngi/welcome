a02503f84e00ab961c7486b495298c105ba39632
MonikaSoftEngi <ahermonika31@gmail.com>
2024-09-28 11:08:12 +0530

    1.Hook use in react.

--- src/componets/shortCircuit.jsx
+++ src/componets/shortCircuit.jsx
@@ -3,9 +3,16 @@ import {useState} from "react";
 const ShortCircuitExample = ()  =>  {
     const[isLoggedIn,setIsLoggedIn] = useState(true)
     const [user,setUser] = useState("");
+    
+    // hooks use
+    const [count,setcount] = useStete(0);
+
+    if (user) {
+setCount(count + 1)
+    }
     return (
         <section className="container short-container">
-<h1>Welcome to the ShortCurcuit Evaluation</h1>
+<h1>Welcome to the ShortCircuit Evaluation</h1>
 {isLoggedIn && <p>You are Logged in!</p>}
        
        {user ?  `Hello ${user}`:"plz Log in!"}
