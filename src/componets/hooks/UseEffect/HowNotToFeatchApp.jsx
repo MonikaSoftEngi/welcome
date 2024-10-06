e3a2b5bcbd13b69c5c1c4577bbc2b4dca67a1307
MonikaSoftEngi <ahermonika31@gmail.com>
2024-10-06 15:20:33 +0530

    1.fetching dada  api .

--- src/componets/hooks/UseEffect/HowNotToFeatchApp.jsx
+++ src/componets/hooks/UseEffect/HowNotToFeatchApp.jsx
@@ -1,23 +1,50 @@
 import { useState } from "react";
 import { useState } from "react";
 import "./Project/Pokemon.css";
+import { useEffect } from "react";
 
 export const HownotToFetchApi = () => {
-  const [apiDada, setApiData] = useState([]);
-  fetch("https://jsonplaceholder.typicode.com/posts")
-    .then((res) => res.json())
-    .then((data) => {
-        // setApiData(data);
-    })
-    .catch((error) => Console.log(error));
+  const [apiDada, setApiData] = useState([null]);
+
+  const API = "https://pokeapi.co/api/v2/pokemon/pikachu";
+
+   const  feachPokkemon = () => {
+    fetch(API)
+      .then((res) => res.json())
+      .then((data) => {
+    setApiData(data);
+      })
+
+  .catch((error) => Console.log(error));
+   }
+
+  useEffect(() => {
+    fetchPokemon()
+},[]);
+
+console.log(apiData);
+if (apiData)  {
+
+}
+
+
   return (
-    <div className="container effect-container">
-      <ul>
-        data:
-        {setApiData.map((curData) => {
-          return <li key={curData.id}>{curData.title}</li>;
-        })}
-      </ul>
-    </div>
-  );
+<section className="container">
+    <header>
+        <h1>Lets Catch Pokemon</h1>
+    </header>
+    <ul className="card-demo">
+        <li className="Pokemon-card">
+          <figure>
+            <img
+            src={apiData.sprites.other.dream_world.front_default}
+            alt={apiData.name}
+            className="pokemon-image"
+            />
+            <h1>{apiData.name}</h1>
+          </figure>
+        </li>
+    </ul>
+</section>
+ );
 };
