121fd4695c85beb63482b692c5a62ad6b66b0666
MonikaSoftEngi <ahermonika31@gmail.com>
2024-10-06 20:13:49 +0530

    1.Handling Loading and Error states.

--- src/componets/hooks/UseEffect/HowNotToFeatchApp.jsx
+++ src/componets/hooks/UseEffect/HowNotToFeatchApp.jsx
@@ -5,46 +5,74 @@ import { useEffect } from "react";
 
 export const HownotToFetchApi = () => {
   const [apiDada, setApiData] = useState([null]);
-
+  const [Loading, setLoading] = useState(true);
+const [error, setError] = useState(null);
   const API = "https://pokeapi.co/api/v2/pokemon/pikachu";
 
-   const  feachPokkemon = () => {
+  const feachPokkemon = () => {
     fetch(API)
       .then((res) => res.json())
       .then((data) => {
-    setApiData(data);
+        setApiData(data);
+        setLoading(false);
       })
 
-  .catch((error) => Console.log(error));
-   }
+      .catch((error) => {
+        Console.log(error);
+        setError(error);
+        setLoading(false);
+      });
+  };
 
   useEffect(() => {
-    fetchPokemon()
-},[]);
+    fetchPokemon();
+  }, []);
 
-console.log(apiData);
-if (apiData)  {
+  console.log(Pokemon);
 
-}
-
+  if (Loading) 
+    return (
+<div>
+    <h1>
+        Loading....
+    </h1>
+</div>
+);
+   if (error)
+    return (
+<div>
+<h1>Error: {error.message} </h1>
+</div>
+);
 
   return (
-<section className="container">
-    <header>
+    <section className="container">
+      <header>
         <h1>Lets Catch Pokemon</h1>
-    </header>
-    <ul className="card-demo">
+      </header>
+      <ul className="card-demo">
         <li className="Pokemon-card">
           <figure>
             <img
-            src={apiData.sprites.other.dream_world.front_default}
-            alt={apiData.name}
-            className="pokemon-image"
+              src={Pokemon.sprites.other.dream_world.front_default}
+              alt={Pokemon.name}
+              className="pokemon-image"
             />
-            <h1>{apiData.name}</h1>
-          </figure>
+            </figure>
+            <h1>{Pokemon.name}</h1>
+           <div className="grid-three-cols">
+            <p className="Pokemon-info">
+                Height: <span> {pokemon.height} </span>
+            </p>
+            <p className="pokemon-info">
+                Weight: <span> {pokemon.Weight} </span>
+            </p>
+            <p className="pokemon-info">
+                speed: <span>{pokeman.stats[5].base-stat} </span>
+            </p>
+           </div>
         </li>
-    </ul>
-</section>
- );
+      </ul>
+    </section>
+  );
 };
