5ce882647caf8913f73ae6112d2447c74d785d74
MonikaSoftEngi <ahermonika31@gmail.com>
2024-10-16 18:12:02 +0530

    1.Navigation Bar.

--- src/componets/Layout/Header.jsx
+++ src/componets/Layout/Header.jsx
@@ -1,3 +1,52 @@
+import {NavLink} from "react-router-dom";
+
 export const Header = () => {
-    return <h1>Header</h1>;
-}
+  return (
+    <>
+      <header className="section-navbar">
+        <section className="top_text">
+          <div className="head container">
+            <div className="head-txt">
+              <p>Get Thapa Membership,30-day return or refund guarantea.</p>
+            </div>
+            <div className="sing-in-up">
+              <NavLink to="#">SIGN IN</NavLink>
+              <NavLink to="#">SIGN UP</NavLink>
+            </div>
+          </div>
+        </section>
+
+        <div className="container">
+          <div className="navbar-brand">
+            <NavLink to="index">
+              <p>ThapaFlix</p>
+            </NavLink>
+          </div>
+
+          <nav className="navbar">
+            <ul>
+              <li className="nav-bar">
+                <NavLink to="/" className="nav-link">
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/about">about</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="movie" className="nav-link">
+                  movies
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="contact" className="nav-link">
+                  contact
+                </NavLink>
+              </li>
+             </ul>
+           </nav>
+        </div>
+      </header>
+    </>
+  );
+};
