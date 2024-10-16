e673218de6c8e1af60d222103b3b6659cef8277f
MonikaSoftEngi <ahermonika31@gmail.com>
2024-10-16 17:00:59 +0530

    1.App Layout.

--- src/App.jsx
+++ src/App.jsx
@@ -300,46 +300,57 @@
 //   );
 // };
 
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+import { createBrowserRouter,  RouterProvider } from "react-router-dom";
 import { Home } from "./Pages/Home";
 import { About } from "./Pages/About";
 import { Movie } from "./Pages/Movie";
 import { Contact } from "./Pages/Contact";
+import AppLayout from "./componets/Layout/AppLayout";
 const App = () => {
-  // const router = createBrowserRouter([
-  //   {
-  //     path: "/",
-  //     element: <Home />,
-  //   },
+  const router = createBrowserRouter([
 
-  //   {
-  //     path: "/about",
-  //     element: <About />,
-  //     }, 
-
-     
-  //     {
-  //     path: "/movie",
-  //     element: <Movie />,
-  //   },
-  //   {
-  //     path: "/contact",
-  //     element: <Contact />,
-  //   },
-  // ]);
-
-
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route>
-        <Route  path="/"  element={<Home />} />
-        <Route  path="/about"  element={<About />} />
-        <Route  path="/movie"  element={<Movie />} /> 
-        <Route  path="/contact" element={<Contact />} />
-       </Route>
+    {
+      path:"/",
+      element:<AppLayout />,
+      children:[
+        {
+          
+            path: "/",
+            element: <Home />,
+          },
       
-    )
-  );
+          {
+            path: "/about",
+            element: <About />,
+            }, 
+      
+           
+            {
+            path: "/movie",
+            element: <Movie />,
+          },
+          {
+            path: "/contact",
+            element: <Contact />,
+          },
+        
+      ]
+    },
+    
+  ]);
+
+
+//   const router = createBrowserRouter(
+//     createRoutesFromElements(
+//       <Route>
+//         <Route  path="/"  element={<Home />} />
+//         <Route  path="/about"  element={<About />} />
+//         <Route  path="/movie"  element={<Movie />} /> 
+//         <Route  path="/contact" element={<Contact />} />
+//        </Route>
+      
+//     )
+//   );
 
  return <RouterProvider router={router} />;
 };
