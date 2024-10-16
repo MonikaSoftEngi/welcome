43af213f4dbba51b71b70ee678e26c772da47dfc
MonikaSoftEngi <ahermonika31@gmail.com>
2024-10-16 11:51:15 +0530

    1.Router setup.

--- src/App.jsx
+++ src/App.jsx
@@ -300,34 +300,48 @@
 //   );
 // };
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
 import { Home } from "./Pages/Home";
 import { About } from "./Pages/About";
 import { Movie } from "./Pages/Movie";
 import { Contact } from "./Pages/Contact";
 const App = () => {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Home />,
-    },
+  // const router = createBrowserRouter([
+  //   {
+  //     path: "/",
+  //     element: <Home />,
+  //   },
 
-    {
-      path: "/about",
-      element: <About />,
-    },
+  //   {
+  //     path: "/about",
+  //     element: <About />,
+  //     }, 
 
-    {
-      path: "/movie",
-      element: <Movie />,
-    },
-    {
-      path: "/contact",
-      element: <Contact />,
-    },
-  ]);
+     
+  //     {
+  //     path: "/movie",
+  //     element: <Movie />,
+  //   },
+  //   {
+  //     path: "/contact",
+  //     element: <Contact />,
+  //   },
+  // ]);
 
-  return <RouterProvider router={router} />;
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+        <Route  path="/"  element={<Home />} />
+        <Route  path="/about"  element={<About />} />
+        <Route  path="/movie"  element={<Movie />} /> 
+        <Route  path="/contact" element={<Contact />} />
+       </Route>
+      
+    )
+  );
+
+ return <RouterProvider router={router} />;
 };
 
 export default App;
