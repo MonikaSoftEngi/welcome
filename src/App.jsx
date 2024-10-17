f0fbe184a6e05f6b3d2cba3c7d67b6debc8fcca5
MonikaSoftEngi <ahermonika31@gmail.com>
2024-10-17 17:06:10 +0530

    1.HandlingError.'

--- src/App.jsx
+++ src/App.jsx
@@ -300,59 +300,62 @@
 //   );
 // };
 
-import { createBrowserRouter,  RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Home } from "./Pages/Home";
 import { About } from "./Pages/About";
 import { Movie } from "./Pages/Movie";
 import { Contact } from "./Pages/Contact";
 import AppLayout from "./componets/Layout/AppLayout";
+import "./App.css";
+import { ErrorPage } from "./Pages/ErrorPage";
+// import { NotFound } from "./Pages/NotFound";
+
 const App = () => {
   const router = createBrowserRouter([
-
     {
-      path:"/",
-      element:<AppLayout />,
-      children:[
+      path: "/",
+      element: <AppLayout />,
+      errorElement: <ErrorePage />,
+      children: [
         {
-          
-            path: "/",
-            element: <Home />,
-          },
-      
-          {
-            path: "/about",
-            element: <About />,
-            }, 
-      
-           
-            {
-            path: "/movie",
-            element: <Movie />,
-          },
-          {
-            path: "/contact",
-            element: <Contact />,
-          },
-        
-      ]
+          path: "/",
+          element: <Home />,
+        },
+
+        {
+          path: "/about",
+          element: <About />,
+        },
+
+        {
+          path: "/movie",
+          element: <Movie />,
+        },
+        {
+          path: "/contact",
+          element: <Contact />,
+        },
+        // {
+        //   path: " * ",
+        //   element: <NotFound />,
+        // }
+      ],
     },
-    
   ]);
 
+  //   const router = createBrowserRouter(
+  //     createRoutesFromElements(
+  //       <Route>
+  //         <Route  path="/"  element={<Home />} />
+  //         <Route  path="/about"  element={<About />} />
+  //         <Route  path="/movie"  element={<Movie />} />
+  //         <Route  path="/contact" element={<Contact />} />
+  //        </Route>
 
-//   const router = createBrowserRouter(
-//     createRoutesFromElements(
-//       <Route>
-//         <Route  path="/"  element={<Home />} />
-//         <Route  path="/about"  element={<About />} />
-//         <Route  path="/movie"  element={<Movie />} /> 
-//         <Route  path="/contact" element={<Contact />} />
-//        </Route>
-      
-//     )
-//   );
+  //     )
+  //   );
 
- return <RouterProvider router={router} />;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
---
+++ src/Pages/ErrorPage.jsx
@@ -0,0 +1,30 @@
+import {useRouteError} from "react-router-dom";
+
+export const ErrorPage = ()=> {
+    const error = useRouteError();
+    if (error.status === 404){
+        return (
+            <section className="error-section">
+                <div id="error-text">
+                    <figure>
+                        <img
+                        src="http://cdn.dribble.com/user/7222246/screenshotes/30661818/404-page.gif"
+                        alt="404 page"
+                         />
+                    </figure>
+                    <div className="text-center" >
+                        <p className="p-a">
+                            .The page you were Lokking for could not be found
+                        </p>
+                        <p className="p-b">... Back to previous page</p>
+                    </div>
+                </div>
+                <navLink to="/" className="btn">
+                Go To The Home page
+                </navLink>
+            </section>
+        );
+    };
+    console.log(error);
+    return <h1>The page you are Looking does not exist</h1>;
+};
