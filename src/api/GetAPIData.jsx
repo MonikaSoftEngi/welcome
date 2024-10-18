0617c18ab9e0b549f8e5e04611e25d716478eb41
MonikaSoftEngi <ahermonika31@gmail.com>
2024-10-18 18:45:49 +0530

    1. ENV Using react.

--- src/api/GetAPIData.jsx
+++ src/api/GetAPIData.jsx
@@ -3,7 +3,7 @@
 export const getMoviesData =  async ()  => {
  try {
     const response = await fetch(
-        "http://www.ombapi.com/?i=tt3896198&apikey=1c12799f&s=titanic&page=1"
+        `http://www.ombapi.com/?i=tt3896198&apikey=${import.meta.env.VITE_API_KEY}&s=titanic&page=1`
     );
 const data  = resopnse.json();
 return data;
