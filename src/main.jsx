7ed36b12585d9bef7959a788072704c5952be38e
MonikaSoftEngi <ahermonika31@gmail.com>
2025-01-12 00:26:18 +0530

    1.redux dispatch getstate.

--- src/main.jsx
+++ src/main.jsx
@@ -16,6 +16,7 @@ import { StrictMode} from "react";
 import  {createRoot} from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import "./store.jsx";
 createRoot(document.getElementById("root")).render(
         <StrictMode>
                 <App />
--- src/store.jsx
+++ src/store.jsx
@@ -1,7 +1,12 @@
+import {createStore} from "redux";
+
+
+
 const ADD_TASK ="task/add";
 const DELETE_TASK ="task/DELETE";
 const initialState  = {
     task: [],
+    isLoaing:false,
 };
 const taskReducer = (State = initialState, action) => {
   switch (action.type) {
@@ -18,9 +23,27 @@ const taskReducer = (State = initialState, action) => {
             })
         return {
             ...State,
+
             task: [...State.task,updatedTask],
         };
         default:
             return State;
   }
 };
+
+// create the redex store using te reducer
+const store = createStore(taskReducer);
+console.log();
+console.group("initial  State:",store.getstate());
+
+store.dispatch({type:ADD_TASK,payload:"Buy ThapaTechnical Code"});
+console.log("updated State:",store.getstate());
+
+
+store.dispatch({type:ADD_TASK,payload:"Buy Mango"});
+console.log("updated State:",store.getstate());
+
+store.dispatch({type:DELETE_TASK,payload:1});
+console.log("delete State:",store.getState());
+
+
