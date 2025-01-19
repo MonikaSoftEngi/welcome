6c1835f69875696fe01431e63c9c43750477c533
MonikaSoftEngi <ahermonika31@gmail.com>
2025-01-19 23:51:32 +0530

    1.RTK createSlice."

--- src/App.jsx
+++ src/App.jsx
@@ -589,91 +589,103 @@ import { useState } from "react";
 
 
 // cureency converter******************
-  // import { useState } from "react";
-  import {useQuery} from "@tanstack/react-query";
-  import {CurrencyConverter} from "./api/PostApi";
-const App = ()  => {
-  const [amount, setAmount] = useState(0);
-  const [fromCurrency, setFromCurrency] = useState("USD");
-  const [toCurrency,setToCurrency] = useState("INR");
+  
+//   import {useQuery} from "@tanstack/react-query";
+//   import {CurrencyConverter} from "./api/PostApi";
+// const App = ()  => {
+//   const [amount, setAmount] = useState(0);
+//   const [fromCurrency, setFromCurrency] = useState("USD");
+//   const [toCurrency,setToCurrency] = useState("INR");
 
-   const {data :ConvertedAmount
-    ,isloading,error,
-  refetch,
-} = useQuery({
-    querykey:['currency'],
-    queryFn:() => CurrencyConverter(fromCurrency, toCurrency,
-      amount),
-      enabled:false
-  });
-  const handleCurrencyConverter = () => {
-if (amount > 0) {
-  refetch();
-}
+//    const {data :ConvertedAmount
+//     ,isloading,error,
+//   refetch,
+// } = useQuery({
+//     querykey:['currency'],
+//     queryFn:() => CurrencyConverter(fromCurrency, toCurrency,
+//       amount),
+//       enabled:false
+//   });
+//   const handleCurrencyConverter = () => {
+// if (amount > 0) {
+//   refetch();
+// }
 
     
-  };
-return (
-<section className="currency-converter">
-<div className="currency-converter">
-  <h>Hello currency</h>
-  <hr />
-<div> 
-  <label>
-    Amount:
-    <input type="text" value={amount}
-    onChange={(e)=> setAmount(e.target.value)} />
-  </label>
-</div>
-<section className="currency-selectour">
-<label>
-  from:
-  <select value= {fromCurrency}
-  onChange={(e) => setFromCurrency(e.target.value)}
-  >
-    {[  "INR", "USD" , "EUR" , "GBP", "AUD"].map((currency)=> {
-      return (
-        <option  key={currency}  value= {currency}>
-        </option>
-      );
-    })}
-  </select>
-</label>
+//   };
+// return (
+// <section className="currency-converter">
+// <div className="currency-converter">
+//   <h>Hello currency</h>
+//   <hr />
+// <div> 
+//   <label>
+//     Amount:
+//     <input type="text" value={amount}
+//     onChange={(e)=> setAmount(e.target.value)} />
+//   </label>
+// </div>
+// <section className="currency-selectour">
+// <label>
+//   from:
+//   <select value= {fromCurrency}
+//   onChange={(e) => setFromCurrency(e.target.value)}
+//   >
+//     {[  "INR", "USD" , "EUR" , "GBP", "AUD"].map((currency)=> {
+//       return (
+//         <option  key={currency}  value= {currency}>
+//         </option>
+//       );
+//     })}
+//   </select>
+// </label>
 
-<label>
-from:
-  <select value= {toCurrency}
-  onChange={(e) => setFromCurrency(e.target.value)}
-  >
-    {[  "INR", "USD" , "EUR" , "GBP", "AUD"].map((currency)=> {
-      return (
-        <option  key={currency}  value= {currency}>
-        </option>
-      );
-    })}
-  </select>
-</label>
-</section>
-<button disabled= {isloading || amount <= 0}
-onClick={handleCurrencyConverter}
-> 
-  {isloading ? "converting.." :"convert" }
-  </button>
+// <label>
+// from:
+//   <select value= {toCurrency}
+//   onChange={(e) => setFromCurrency(e.target.value)}
+//   >
+//     {[  "INR", "USD" , "EUR" , "GBP", "AUD"].map((currency)=> {
+//       return (
+//         <option  key={currency}  value= {currency}>
+//         </option>
+//       );
+//     })}
+//   </select>
+// </label>
+// </section>
+// <button disabled= {isloading || amount <= 0}
+// onClick={handleCurrencyConverter}
+// > 
+//   {isloading ? "converting.." :"convert" }
+//   </button>
 
-  <hr />
-  {
-    ConvertedAmount && (
-      <h2>
-        {amount} {fromCurrency}  = {ConvertedAmount.toFixed(2)}{toCurrency}
-      </h2>
-    )
-  }
-  {error && <p>An occurred: {error.message}</p>}
-</div>
-</section>
+//   <hr />
+//   {
+//     ConvertedAmount && (
+//       <h2>
+//         {amount} {fromCurrency}  = {ConvertedAmount.toFixed(2)}{toCurrency}
+//       </h2>
+//     )
+//   }
+//   {error && <p>An occurred: {error.message}</p>}
+// </div>
+// </section>
 
 
-);
-};
+// );
+// };
 
-export default App;
+// export default App;
+
+
+
+// RTK Slice************
+
+
+
+
+
+
+
+
--- src/store.jsx
+++ src/store.jsx
@@ -1,101 +1,130 @@
 // import { applyMiddleware, createStore } from "redux";
 // import { composewithDevTools } from "@redux-devtools/extension";
 // import { thunk } from "redux-thunk";
-import { configureStore } from "@reduxjs/toolkit";
+// import { configureStore } from "@reduxjs/toolkit";
 
-const ADD_TASK = "task/add";
-const DELETE_TASK = "task/DELETE";
-const FETCH_TASKS = "task/fetch";
+// const ADD_TASK = "task/add";
+// const DELETE_TASK = "task/DELETE";
+// const FETCH_TASKS = "task/fetch";
 
+// const initialState = {
+//   task: [],
+// };
+// const taskReducer = (State = initialState, action) => {
+//   switch (action.type) {
+//     case ADD_TASK:
+//       return {
+//         ...State,
+//         task: [...State.task, action.payload],
+//       };
+//     case DELETE_TASK:
+//       const updatedTask = State.task.filter((curTask, index) => {
+//         return index == action.payload;
+//       });
+//       return {
+//         ...State,
+
+//         task: [...State.task, updatedTask],
+//       };
+
+//     case FETCH_TASKS:
+//       return {
+//         ...State,
+//         task: [...State.task, ...action.payload],
+//       };
+//     default:
+//       return State;
+//   }
+// };
+
+// // create the redex store using te reducer
+
+// // const store = createStore(
+// //   taskReducer,
+// //   composewithDevTools(applyMiddleware(thunk))
+// // );
+// // console.log();
+
+
+//  export const store = configureStore({
+//   reducer:{
+//     taskReducer,
+//   },
+// });
+
+
+
+
+
+// console.group("initial  State:", store.getState());
+
+// store.dispatch(addTask("Buy TT code"));
+// console.log("updated State:", store.getState());
+
+// store.dispatch(addTask("Buy Mango"));
+// console.log("updated State:", store.getState());
+
+// store.dispatch({ type: DELETE_TASK, payload: 1 });
+// console.log("delete State:", store.getState());
+
+// export const addTask = (data) => {
+//   return { type: ADD_TASK, payload: "data" };
+// };
+
+// export const deleteTask = (id) => {
+//   return { type: DELETE_TASK, payload: id };
+// };
+// store.dispatch(addTask("Buy TT code"));
+// store.dispatch(addTask("Buy apple"));
+// store.dispatch(addTask("Buy banana"));
+// console.log("updated State:", store.getState());
+
+// store.dispatch(addTask("Buy Mango"));
+
+// console.log("update State:", store.getState);
+
+// export const fetchTask = () => {
+//   return async (dispatch) => {
+//     try {
+//       const res = await fetch(
+//         "https://jsonplaceholder.typicode.com/todos?_limit=3"
+//       );
+//       const task = await res.json();
+//       dispatch({
+//         type: FETCH_TASKS,
+//         payload: task.map((curTask) => curTask.title),
+//       });
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
+// };
+
+
+
+
+
+// RTK slice*******
+import {ConfigureStore, createSlice} from " @reduxjs/toolkit";
 const initialState = {
   task: [],
-};
-const taskReducer = (State = initialState, action) => {
-  switch (action.type) {
-    case ADD_TASK:
-      return {
-        ...State,
-        task: [...State.task, action.payload],
-      };
-    case DELETE_TASK:
-      const updatedTask = State.task.filter((curTask, index) => {
-        return index == action.payload;
-      });
-      return {
-        ...State,
-
-        task: [...State.task, updatedTask],
-      };
-
-    case FETCH_TASKS:
-      return {
-        ...State,
-        task: [...State.task, ...action.payload],
-      };
-    default:
-      return State;
-  }
-};
-
-// create the redex store using te reducer
-
-// const store = createStore(
-//   taskReducer,
-//   composewithDevTools(applyMiddleware(thunk))
-// );
-// console.log();
-
-
- export const store = configureStore({
-  reducer:{
-    taskReducer,
+ };
+ 
+ //?RTK slice
+   const taskReducer =  createSlice({
+  name:"task",
+  initialState,
+  reducers:{
+ addTask(state,action) {},
+ deleteTask(state,action) {},
   },
-});
-
-
-
-
-
-console.group("initial  State:", store.getState());
-
-store.dispatch(addTask("Buy TT code"));
-console.log("updated State:", store.getState());
-
-store.dispatch(addTask("Buy Mango"));
-console.log("updated State:", store.getState());
-
-store.dispatch({ type: DELETE_TASK, payload: 1 });
-console.log("delete State:", store.getState());
-
-export const addTask = (data) => {
-  return { type: ADD_TASK, payload: "data" };
-};
-
-export const deleteTask = (id) => {
-  return { type: DELETE_TASK, payload: id };
-};
-store.dispatch(addTask("Buy TT code"));
-store.dispatch(addTask("Buy apple"));
-store.dispatch(addTask("Buy banana"));
-console.log("updated State:", store.getState());
-
-store.dispatch(addTask("Buy Mango"));
-
-console.log("update State:", store.getState);
-
-export const fetchTask = () => {
-  return async (dispatch) => {
-    try {
-      const res = await fetch(
-        "https://jsonplaceholder.typicode.com/todos?_limit=3"
-      );
-      const task = await res.json();
-      dispatch({
-        type: FETCH_TASKS,
-        payload: task.map((curTask) => curTask.title),
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  };
-};
+ });
+ console.log(taskReducer);
+ 
+ // new style
+ export const store = ConfigureStore({
+   reducer: {
+     taskReducer,
+   },
+ });
+ 
